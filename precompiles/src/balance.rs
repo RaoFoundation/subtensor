2541dc4aa5397f246eb8c4cf26ca666cb00dce40
UnArbosSix <arbossix@bittensor.com>
2026-07-15 08:23:45 -0700

    expand tests

--- precompiles/src/balance.rs
+++ precompiles/src/balance.rs
@@ -39,14 +39,32 @@ mod tests {
     #![allow(clippy::arithmetic_side_effects)]
 
     use super::*;
+    use crate::Precompiles;
     use crate::mock::{
-        AccountId, Runtime, addr_from_index, assert_static_call, fund_account, new_test_ext,
-        precompiles, selector_u32,
+        AccountId, Runtime, TestBalanceStatus, abi_word, addr_from_index, execute_precompile,
+        fund_account, new_test_ext, precompiles, selector_u32,
     };
+    use frame_support::traits::{
+        ReservableCurrency,
+        tokens::{
+            Fortitude, Preservation,
+            fungible::{Inspect, InspectFreeze, InspectHold, MutateFreeze, MutateHold},
+        },
+    };
+    use pallet_admin_utils::{PrecompileEnable, PrecompileEnum};
+    use precompile_utils::prelude::RuntimeHelper;
     use precompile_utils::solidity::encode_with_selector;
+    use precompile_utils::testing::PrecompileTesterExt;
 
     fn coldkey(byte: u8) -> AccountId {
         AccountId::from([byte; 32])
+    }
+
+    fn balance_call_input(coldkey: &AccountId) -> Vec<u8> {
+        encode_with_selector(
+            selector_u32("getFreeBalance(bytes32)"),
+            (H256::from_slice(coldkey.as_ref()),),
+        )
     }
 
     #[test]
@@ -57,16 +75,15 @@ mod tests {
             let amount = 123_456_789_u64;
             fund_account(&target, amount);
 
-            assert_static_call(
-                &precompiles::<BalancePrecompile<Runtime>>(),
-                caller,
-                addr_from_index(BalancePrecompile::<Runtime>::INDEX),
-                encode_with_selector(
-                    selector_u32("getFreeBalance(bytes32)"),
-                    (H256::from_slice(target.as_ref()),),
-                ),
-                U256::from(amount),
-            );
+            precompiles::<BalancePrecompile<Runtime>>()
+                .prepare_test(
+                    caller,
+                    addr_from_index(BalancePrecompile::<Runtime>::INDEX),
+                    balance_call_input(&target),
+                )
+                .with_static_call(true)
+                .expect_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())
+                .execute_returns_raw(abi_word(U256::from(amount)));
         });
     }
 
@@ -76,16 +93,148 @@ mod tests {
             let caller = addr_from_index(0x7001);
             let target = coldkey(0x22);
 
-            assert_static_call(
-                &precompiles::<BalancePrecompile<Runtime>>(),
-                caller,
-                addr_from_index(BalancePrecompile::<Runtime>::INDEX),
-                encode_with_selector(
-                    selector_u32("getFreeBalance(bytes32)"),
-                    (H256::from_slice(target.as_ref()),),
+            precompiles::<BalancePrecompile<Runtime>>()
+                .prepare_test(
+                    caller,
+                    addr_from_index(BalancePrecompile::<Runtime>::INDEX),
+                    balance_call_input(&target),
+                )
+                .with_static_call(true)
+                .expect_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())
+                .execute_returns_raw(abi_word(U256::zero()));
+        });
+    }
+
+    #[test]
+    fn balance_precompile_returns_free_not_total_or_reducible_balance() {
+        new_test_ext().execute_with(|| {
+            let caller = addr_from_index(0x7001);
+            let target = coldkey(0x33);
+            let initial = 1_000_u64;
+            let reserved = 100_u64;
+            let held = 200_u64;
+            let frozen = 650_u64;
+            let expected_free = initial - reserved - held;
+
+            fund_account(&target, initial);
+            pallet_balances::Pallet::<Runtime>::reserve(&target, reserved.into())
+                .expect("reserve should succeed for funded account");
+            <pallet_balances::Pallet<Runtime> as MutateHold<AccountId>>::hold(
+                &TestBalanceStatus::Test,
+                &target,
+                held.into(),
+            )
+            .expect("hold should succeed for funded account");
+            <pallet_balances::Pallet<Runtime> as MutateFreeze<AccountId>>::set_freeze(
+                &TestBalanceStatus::Test,
+                &target,
+                frozen.into(),
+            )
+            .expect("freeze should succeed for funded account");
+
+            assert_eq!(
+                pallet_balances::Pallet::<Runtime>::reserved_balance(&target),
+                (reserved + held).into()
+            );
+            assert_eq!(
+                <pallet_balances::Pallet<Runtime> as InspectHold<AccountId>>::balance_on_hold(
+                    &TestBalanceStatus::Test,
+                    &target,
                 ),
+                held.into()
+            );
+            assert_eq!(
+                <pallet_balances::Pallet<Runtime> as InspectFreeze<AccountId>>::balance_frozen(
+                    &TestBalanceStatus::Test,
+                    &target,
+                ),
+                frozen.into()
+            );
+            assert_eq!(
+                pallet_balances::Pallet::<Runtime>::total_balance(&target),
+                initial.into()
+            );
+            assert_eq!(
+                <pallet_balances::Pallet<Runtime> as Inspect<AccountId>>::reducible_balance(
+                    &target,
+                    Preservation::Expendable,
+                    Fortitude::Polite,
+                ),
+                350_u64.into()
+            );
+            assert_eq!(
+                pallet_balances::Pallet::<Runtime>::free_balance(&target),
+                expected_free.into()
+            );
+
+            precompiles::<BalancePrecompile<Runtime>>()
+                .prepare_test(
+                    caller,
+                    addr_from_index(BalancePrecompile::<Runtime>::INDEX),
+                    balance_call_input(&target),
+                )
+                .with_static_call(true)
+                .expect_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())
+                .execute_returns_raw(abi_word(U256::from(expected_free)));
+        });
+    }
+
+    #[test]
+    fn balance_precompile_rejects_malformed_calldata() {
+        new_test_ext().execute_with(|| {
+            let caller = addr_from_index(0x7001);
+            let precompile_addr = addr_from_index(BalancePrecompile::<Runtime>::INDEX);
+            let mut malformed = selector_u32("getFreeBalance(bytes32)")
+                .to_be_bytes()
+                .to_vec();
+            malformed.extend_from_slice(&[0u8; 31]);
+
+            let result = execute_precompile(
+                &precompiles::<BalancePrecompile<Runtime>>(),
+                precompile_addr,
+                caller,
+                malformed,
                 U256::zero(),
             );
+
+            assert!(
+                matches!(result, Some(Err(_))),
+                "malformed calldata must not return a balance"
+            );
+        });
+    }
+
+    #[test]
+    fn balance_precompile_respects_enable_disable_administration() {
+        new_test_ext().execute_with(|| {
+            let caller = addr_from_index(0x7001);
+            let target = coldkey(0x44);
+            let amount = 42_u64;
+            let precompile_addr = addr_from_index(BalancePrecompile::<Runtime>::INDEX);
+            let input = balance_call_input(&target);
+            let precompiles = Precompiles::<Runtime>::new();
+
+            fund_account(&target, amount);
+
+            PrecompileEnable::<Runtime>::insert(PrecompileEnum::AccountBalance, false);
+            let disabled = execute_precompile(
+                &precompiles,
+                precompile_addr,
+                caller,
+                input.clone(),
+                U256::zero(),
+            );
+            assert!(
+                matches!(disabled, Some(Err(_))),
+                "disabled account-balance precompile must reject calls"
+            );
+
+            PrecompileEnable::<Runtime>::insert(PrecompileEnum::AccountBalance, true);
+            precompiles
+                .prepare_test(caller, precompile_addr, input)
+                .with_static_call(true)
+                .expect_cost(RuntimeHelper::<Runtime>::db_read_gas_cost())
+                .execute_returns_raw(abi_word(U256::from(amount)));
         });
     }
 }
