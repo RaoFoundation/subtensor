fa661f65b6fd13b2a0465ce24246a82294add886
UnArbosFive <arbosfive@bittensor.com>
2026-08-14 16:45:47 +0200

    test(grandpa): satisfy strict clippy lints

--- node/src/service/grandpa_warp_sync.rs
+++ node/src/service/grandpa_warp_sync.rs
@@ -276,10 +276,14 @@ mod tests {
         let encoded = hex_literal::hex!(
             "2c320c00000000004d643da5fd7cd2b9ceb795091643e7223819e2a01f942ac049c5b928f7e30dc4f3725400144d643da5fd7cd2b9ceb795091643e7223819e2a01f942ac049c5b928f7e30dc4f3725400072a8a1a2567ca0633ac6497e47c95067ea208eb125b43664129aed09ec7e5c055a4bf320d879b4c0ed644c539a10ffa9c61eb2a464d78fd04950081f0aa7501755f75dfc66aaa3b1e761a8845249509b8bd2fdf0d94cb74e1e12e1e0f4d35194d643da5fd7cd2b9ceb795091643e7223819e2a01f942ac049c5b928f7e30dc4f37254000a97aafb8a64e0bb41f82cb83badfa6c882433b3acfe03d070f8ffc8d4164dc94611045932a1396ec3a893275b561dba13f9ff1968574d0b93d08ce56b069807b57a038c9139a060358f3b654df74a1cb6d15bcdb8438bcebd64ce67ec4301eb4d643da5fd7cd2b9ceb795091643e7223819e2a01f942ac049c5b928f7e30dc4f3725400971a2c21924c9723121767be46be8e7bac1099aadd6e19401a85e8fc5238364823fb92235535716426f13ef469c1c07808d5125cfb79f65400f1c407bb39e80bc8a00ef71912b3868b101cb70ebd029999d1c9b6a1390122a98f60d72b9a0fc44d643da5fd7cd2b9ceb795091643e7223819e2a01f942ac049c5b928f7e30dc4f37254009469bd1c254becf32318bb5bc96c98d0dfa4fe652613e129f2edb9ed4a8904c9b5f26ef22b09ac25339680578ef9116f0b79d6a6d7701292b57b8572869a3702dc832c3b7bdfc721e90e5ee9e532c06b62a0def3c79dab5324460d938db6600a4d643da5fd7cd2b9ceb795091643e7223819e2a01f942ac049c5b928f7e30dc4f37254002e2bd65267a52080e7635e82dd5dbf891b42549598e244b7ff0e01b88a5187796133bc48bd7713b1025d625f56811a134fbbae38e85b01082b7557755c085602ee70f7b52998c2b4f3d42e509e8360cda92b0cd4ca100cd4d32be5a1ac29790900"
         );
-        let justification =
-            GrandpaJustification::<Block>::decode_all(&mut encoded.as_slice()).unwrap();
+        let Ok(justification) = GrandpaJustification::<Block>::decode_all(&mut encoded.as_slice())
+        else {
+            panic!("stored testnet checkpoint justification must decode");
+        };
         let checkpoints = testnet_checkpoints();
-        let checkpoint = &checkpoints[1];
+        let [_, checkpoint] = checkpoints.as_slice() else {
+            panic!("expected exactly two testnet warp checkpoints");
+        };
 
         assert!(
             justification
